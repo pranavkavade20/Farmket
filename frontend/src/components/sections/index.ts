1ac6b0b15315920ebafd012a2d7e47cb9e900c78
Pranav <pranavkavade9@gmail.com>
2026-08-09 18:52:37 +0530

    feat: add index file for centralized component exports in sections directory

--- frontend/src/components/sections/index.ts
+++ frontend/src/components/sections/index.ts
@@ -3,16 +3,11 @@ export { SocialProofSection } from './SocialProofSection';
 export { ProblemSolutionSection } from './ProblemSolutionSection';
 export { FeaturesBentoSection } from './FeaturesBentoSection';
 export { HowItWorksSection } from './HowItWorksSection';
-export { ProductShowcaseSection } from './ProductShowcaseSection';
 export { TrustSecuritySection } from './TrustSecuritySection';
 export { SuccessStoriesSection } from './SuccessStoriesSection';
-export { MarketplaceActivitySection } from './MarketplaceActivitySection';
 export { FAQSection } from './FAQSection';
 export { CTASection } from './CTASection';
 
-// Keep old exports in case they are used elsewhere
 export { PopularCategories } from './PopularCategories';
 export { ProductGridSection } from './ProductGridSection';
-export { PromoBanners } from './PromoBanners';
 export { WeeklyBestSelling } from './WeeklyBestSelling';
-export { ServicesSection } from './ServicesSection';
